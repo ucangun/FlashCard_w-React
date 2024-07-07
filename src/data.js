a44f284e860ee033e761f3fd6dcce89abcdd07b1
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-07 15:10:57 +0200

    "Updated App.css with new styles and modified data.js to change question IDs"

--- src/data.js
+++ src/data.js
@@ -1,31 +1,31 @@
 const questions = [
   {
-    id: 3457,
+    id: 1,
     question: "What language is React based on?",
     answer: "JavaScript",
   },
   {
-    id: 7336,
+    id: 2,
     question: "What are the building blocks of React apps?",
     answer: "Components",
   },
   {
-    id: 8832,
+    id: 3,
     question: "What's the name of the syntax we use to describe a UI in React?",
     answer: "JSX",
   },
   {
-    id: 1297,
+    id: 4,
     question: "How to pass data from parent to child components?",
     answer: "Props",
   },
   {
-    id: 9103,
+    id: 5,
     question: "How to give components memory?",
     answer: "useState hook",
   },
   {
-    id: 2002,
+    id: 6,
     question:
       "What do we call an input element that is completely synchronised with state?",
     answer: "Controlled element",
