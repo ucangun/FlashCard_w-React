822bb47c7d654a4a554fe3c5a1ee6ec4c1175733
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-07 14:53:27 +0200

    "Added useState and handleClick function to FlashCards component"

--- src/FlashCards.jsx
+++ src/FlashCards.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 const FlashCards = ({ data }) => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
   return (
     <div className="flashcards">
       {data.map((item) => (
-        <div key={item.id}>
+        <div onClick={() => handleClick(item.id)} key={item.id}>
           <p>{item.question}</p>
         </div>
       ))}
